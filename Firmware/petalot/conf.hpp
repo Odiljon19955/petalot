a99af5db42a900e6e5c58dffabad16975ca1ce67
Odiljon19955 <Yason24088@gmail.com>
2025-09-26 17:44:35 +0500

    Update conf.hpp
    300ga

--- Firmware/petalot/conf.hpp
+++ Firmware/petalot/conf.hpp
@@ -81,10 +81,10 @@ void  resetConfiguration(){
     Serial.println("reset");
     strcpy(ssid, "");         
     strcpy(password, "");
-    To = 230;
+    To = 300;
     Vo = 40;
     Fe = true;
-    Tm = 240;
+    Tm = 300;
     Kp = 23.0;
     Ki = 0.043;
     Kd = 160.0;
@@ -160,7 +160,7 @@ void loadConfiguration(bool reset=false) {
   Vo = doc["Vo"] | 40;
   //Vo = Vco;
   Fe = doc["Fe"];
-  Tm = doc["Tm"] | 230;
+  Tm = doc["Tm"] | 300;
   Kp = doc["Kp"]?doc["Kp"].as<double>():23.0;
   Ki = doc["Ki"]?doc["Ki"].as<double>():0.043;
   Kd = doc["Kd"]?doc["Kd"].as<double>():160.0;
